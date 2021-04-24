a8503c05d38e10520a17f2260c91b82a8010de34
alatif8 <ahmed.hlatif@outlook.com>
2021-04-24 04:28:31 +0200

    add comments on Envelope.php

--- src/Envelope.php
+++ src/Envelope.php
@@ -21,11 +21,13 @@ class Envelope{
                 $validate_line_from_env_file = preg_match('~^[a-zA-Z]~',$line);
                 if ($validate_line_from_env_file)
                 {
-                    $env_file_text = explode('= "',$line);
-                    $env_variable_key_length = strpos($env_file_text[0]," ");
-                    $env_variable_key = substr($env_file_text[0],0,$env_variable_key_length);
-                    $env_variable_value_length = strpos($env_file_text[1],"\"");
-                    $env_variable_value = substr($env_file_text[1],0,$env_variable_value_length);
+                    $env_file_text = explode('= "',$line); //split string from .env file to key & value
+                    $env_variable_key_length = strpos($env_file_text[0]," "); //get length of variable associative array key
+                    $env_variable_key = substr($env_file_text[0],0,$env_variable_key_length); //clean key string
+                    $env_variable_value_length = strpos($env_file_text[1],"\""); //get length of variable associative array value
+                    $env_variable_value = substr($env_file_text[1],0,$env_variable_value_length); //clean value string
+
+                    // update variables associative array
                     if ($env_variable_value == true)
                     {
                         $this->variables += [$env_variable_key => $env_variable_value];
@@ -34,7 +36,6 @@ class Envelope{
                     {
                         $this->variables += [$env_variable_key => ""];
                     }
-
                 }
             }
             fclose($file);
